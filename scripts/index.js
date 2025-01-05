5b50cf4d0020ec804137413c9c500ae9936b8f50
Diptendu Sil <diptendusil2012@gmail.com>
2025-01-05 13:41:42 +0530

    feat: design and contact us update

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,4 @@
-const scrollHandler = (_e) => {
+const scrollHandler = () => {
   const dynamicNav = document.querySelector('#dynamic-nav')
   const scrollToTopBtn = document.querySelector('.scroll-to-top')
   if (window.scrollY > 40) {
@@ -15,4 +15,14 @@ const scrollHandler = (_e) => {
     // hide
     scrollToTopBtn.style = 'display: none;'
   }
+}
+
+const scrollToTop = (e) => {
+  window.scrollTo({ top: 0, left: 0, behavior: 'smooth' })
+  scrollHandler()
+}
+
+const scrollToSection = (id) => {
+  document.querySelector(`#${id}`).scrollIntoView({ behavior: 'smooth' })
+  scrollHandler()
 }
